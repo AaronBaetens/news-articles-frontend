a6c70dc900800aa0e5f6563d0f74f957ddee4a0e
Aaron Baetens <aaron.baetens@gmail.com>
2024-01-27 17:12:46 +0100

    feat: added delete dialog

---
+++ src/components/DialogComponent/DialogComponent.jsx
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types"; // Import PropTypes
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from "@mui/material";
+
+const DialogComponent = ({ open, handleClose, title, content, onConfirm }) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="dialog-title"
+      aria-describedby="dialog-description"
+    >
+      <DialogTitle id="dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="dialog-description">{content}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={onConfirm} color="primary" autoFocus>
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+DialogComponent.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  onConfirm: PropTypes.func.isRequired,
+};
+
+DialogComponent.defaultProps = {
+  content: "",
+};
+
+export default DialogComponent;
