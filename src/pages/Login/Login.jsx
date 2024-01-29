d9cd9e85f5da4fe5857b4bd1290f5a35b3e0fada
Aaron Baetens <aaron.baetens@gmail.com>
2024-01-29 16:31:32 +0100

    feat: added login screen ui

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,8 @@ import {
   Typography,
   CircularProgress,
   Link,
+  Container,
+  Paper,
 } from "@mui/material";
 import { useAuth } from "@shared/authentication/useAuth";
 import { loginValidationSchema } from "./validationSchema";
@@ -19,66 +21,93 @@ const Login = () => {
   };
 
   return (
-    <Box
+    <Container
+      component="main"
+      maxWidth={false}
       sx={{
+        height: "100vh",
+        width: "100vw",
         display: "flex",
         flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
+        justifyContent: "center",
+        alignItems: "center", // Center horizontally
+        backgroundImage:
+          'url("https://source.unsplash.com/random/1920x1080?news")',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
       }}
     >
-      <Typography variant="h4" sx={{ mb: 2 }}>
-        Login
-      </Typography>
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        validationSchema={loginValidationSchema}
-        onSubmit={submitLogin}
+      <Paper
+        elevation={6}
+        sx={{
+          p: 4,
+          width: "90%", // Adjust the width to be 90% of the parent
+          maxWidth: 400, // Also set a maximum width
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          borderRadius: 2, // Rounded corners
+        }}
       >
-        {({ isSubmitting, errors, touched }) => (
-          <Form>
-            <Field
-              as={TextField}
-              label="Username"
-              name="username"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              error={touched.username && Boolean(errors.username)}
-              helperText={touched.username && errors.username}
-            />
-            <Field
-              as={TextField}
-              label="Password"
-              name="password"
-              type="password"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              error={touched.password && Boolean(errors.password)}
-              helperText={touched.password && errors.password}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              disabled={isSubmitting || loadingAuth}
-              fullWidth
-            >
-              {loadingAuth ? <CircularProgress size={24} /> : "Login"}
-            </Button>
-            <Box sx={{ mt: 2, textAlign: "center" }}>
-              <Link href="#" variant="body2" sx={{ mr: 2 }}>
-                Forgot Password?
-              </Link>
-              <Link href="#" variant="body2">
-                Sign Up
-              </Link>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-    </Box>
+        <Typography component="h1" variant="h5" sx={{ mb: 3 }}>
+          Login
+        </Typography>
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          validationSchema={loginValidationSchema}
+          onSubmit={submitLogin}
+        >
+          {({ isSubmitting, errors, touched }) => (
+            <Form>
+              <Field
+                as={TextField}
+                label="Username"
+                name="username"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                error={touched.username && Boolean(errors.username)}
+                helperText={touched.username && errors.username}
+              />
+              <Field
+                as={TextField}
+                label="Password"
+                name="password"
+                type="password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={isSubmitting || loadingAuth}
+                fullWidth
+                sx={{ mt: 2, mb: 2 }}
+              >
+                {loadingAuth ? <CircularProgress size={24} /> : "Login"}
+              </Button>
+              <Box sx={{ textAlign: "center" }}>
+                <Link href="#" variant="body2" sx={{ mr: 2 }}>
+                  Forgot Password?
+                </Link>
+                <Link href="#" variant="body2">
+                  Sign Up
+                </Link>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </Paper>
+    </Container>
   );
 };
 
